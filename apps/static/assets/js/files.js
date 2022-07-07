54945bfe44e4ecbd9cafac37993a9909e01f7d1d
impanyu <impanyu@gmail.com>
2022-07-07 00:38:04 -0500

    add to domain 16

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -58,7 +58,7 @@ function add_to_domain(path,file_name){
 
 
 
-  domain_name_select_string = '<select class="form-control" data-toggle="select" title="Simple select" data-placeholder="Select a state">';
+  domain_name_select_string = '<select class="form-control form-select-lg mb-3" data-toggle="select" title="Simple select" data-placeholder="Select a domain" id="domain_name_select">';
    //domain_name_select_string = '<select class="form-select form-select-lg mb-3" aria-label="domain select" id="domain_name_select">';
 
    for(domain_name in domain_names)
