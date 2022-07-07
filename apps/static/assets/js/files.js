b10d433ad82cb1c15e60061143ba027964b1300b
impanyu <impanyu@gmail.com>
2022-07-07 00:30:26 -0500

    add to domain 13

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -1,12 +1,12 @@
 var domain_names=[];
- /* $.get("/get_domains",
+  $.get("/get_domains",
         {
         },function(data,status){
            domain_names = JSON.parse(data);
         }
 
         );
-*/
+
 
 console.info("gogogo");
 
@@ -57,7 +57,10 @@ function add_to_domain(path,file_name){
 
      });
 
-   domain_name_select_string = '<select class="form-select" aria-label="domain select" id="domain_name_select">';
+
+
+
+   domain_name_select_string = '<select class="form-select form-select-lg mb-3" aria-label="domain select" id="domain_name_select">';
 
    for(domain_name in domain_names)
       domain_name_select_string +=  '<option value="'+domain_name+'">'+domain_name+'</option>'
