0acc044ebd50e79308ce7cd55745d7243044e03f
impanyu <impanyu@gmail.com>
2022-07-07 22:48:15 -0500

    domain main page 56

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -157,11 +157,13 @@ function add_to_domain(path,file_name){
         '</div>'+
     '</div>');
 
-    attr_input.getElementById("attr").value = file_name.substr(0,file_name.length-file_name.split(".")[file_name.split(".").length-1].length);
-    domain_and_attr_container = htmlToElement('<div class="row align-items-center" style="width: 1100px; margin-left: 50px;"></div>')
+     domain_and_attr_container = htmlToElement('<div class="row align-items-center" style="width: 1100px; margin-left: 50px;"></div>')
     domain_and_attr_container.appendChild(domain_name_select);
     domain_and_attr_container.appendChild(attr_input);
     add_to_domain_box.appendChild(domain_and_attr_container);
+
+    document.getElementById("attr").value = file_name.substr(0,file_name.length-file_name.split(".")[file_name.split(".").length-1].length);
+
 
 
     date_picker = htmlToElement('<div class="input-daterange datepicker row align-items-center">'+
