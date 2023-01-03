2b1f9ee962d94795e5a5eec266177c9227dd2c0e
impanyu <impanyu>
2023-01-02 22:09:23 -0600

    display data and files on map, each as a point

--- apps/home/views.py
+++ apps/home/views.py
@@ -417,7 +417,7 @@ def data(request):
 
             #query all the data points or files in current path
             data_and_files = open(os.path.join(settings.CORE_DIR, 'data','data_and_files.json'),"r")
-            data_points = json.load(data_and_files)
+            #data_points = json.load(data_and_files)
             #for data_path in data_points:
             #    if data_path.startswith(abs_path) or data_points[data_path]["public"] == True:
             #        response["data_points"].append(data_points[data_path])
