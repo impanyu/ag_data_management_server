ad5ac782828104f4c2c257d9d540f1bb9841ad15
impanyu <impanyu>
2023-01-02 22:33:18 -0600

    display data and files on map, each as a point

--- apps/home/views.py
+++ apps/home/views.py
@@ -421,9 +421,9 @@ def data(request):
             response["data_points"] = []
 
             for data_path in data_points:
-                #print(data_path)
+                print(data_path)
                 #if data_path.startswith(abs_path) or ("public" in data_points[data_path] and data_points[data_path]["public"] == True):
-                response["data_points"].append(data_points[data_path])
+                #response["data_points"].append(data_points[data_path])
 
             response = json.dumps(response)
 
