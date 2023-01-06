8e2989fd6d751105cbd4877ff8ab82f847163cbf
impanyu <impanyu>
2023-01-06 13:19:19 -0600

    display data and files on map, each as a point

--- apps/static/assets/js/scatterplot.js
+++ apps/static/assets/js/scatterplot.js
@@ -1,14 +1,9 @@
 
-// set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 460 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
 // append the svg object to the body of the page
 const svg = d3.select("#files_plot")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", "100%")
+    .attr("height","100%")
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
