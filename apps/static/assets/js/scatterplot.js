cfeb86ea7d249541def8a00b1405f78f0ed7b4fc
impanyu <impanyu>
2023-01-06 13:21:04 -0600

    display data and files on map, each as a point

--- apps/static/assets/js/scatterplot.js
+++ apps/static/assets/js/scatterplot.js
@@ -1,11 +1,16 @@
 
+// set the dimensions and margins of the graph
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
 // append the svg object to the body of the page
 const svg = d3.select("#files_plot")
     .append("svg")
-    .attr("width", "100%")
-    .attr("height","100%")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-    //.attr("transform", `translate(${margin.left}, ${margin.top})`);
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 //Read the data
 d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/2_TwoNum.csv").then( function(data) {
