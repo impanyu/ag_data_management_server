c10ba7a3d67d8697f4f81ed56e0728a7e6f9a4fc
impanyu <impanyu>
2023-01-06 13:32:01 -0600

    display data and files on map, each as a point

--- apps/static/assets/js/scatterplot.js
+++ apps/static/assets/js/scatterplot.js
@@ -1,7 +1,7 @@
 
 // set the dimensions and margins of the graph
-const margin = {top: 10, right: 30, bottom: 30, left: 60},
-        width = 460 - margin.left - margin.right,
+margin = {top: 10, right: 30, bottom: 30, left: 60};
+        width = 460 - margin.left - margin.right;
         height = 400 - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
