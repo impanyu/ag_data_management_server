5c86427d546f487cf6b8f6ae70896a6cdd0402cc
impanyu <impanyu>
2023-01-08 23:30:51 -0600

    create template search.html, display search result on this template

--- apps/home/views.py
+++ apps/home/views.py
@@ -119,7 +119,7 @@ def pages(request):
             context['current_path'] = request.GET['current_path']
             context['segment'] = load_template
 
-            html_template = loader.get_template('home/files.html')
+            html_template = loader.get_template('home/search.html')
             return HttpResponse(html_template.render(context, request))
 
         context['segment'] = load_template
