5830c163b21448ebf2dff5b92af5e30a503eb336
impanyu <impanyu>
2023-01-08 21:32:46 -0600

    display data and files on map, each as a point

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -428,15 +428,7 @@ function get_file_list(){
   $.post("/file_system",
         {
           current_path: current_path
-          /*
-          search_box: document.querySelector("#search_box").value,
-          category: document.querySelector("#category").value,
-          mode: mode,
-          format: format,
-          label: label,
-          time_range: [document.querySelector("#start_date").value,document.querySelector("#end_date").value],
-          bounding_box: [document.querySelector("#southwest").value, document.querySelector("#northeast").value]
-          */
+
 
         },
         function(data, status){
