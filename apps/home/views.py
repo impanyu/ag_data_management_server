2cf1bbe6c01e4d5fe31214b2402c139aa5b1777f
impanyu <impanyu>
2023-01-08 21:41:11 -0600

    display data and files on map, each as a point

--- apps/home/views.py
+++ apps/home/views.py
@@ -447,6 +447,7 @@ def data(request):
 
         elif load_template == 'file_system':
             file_path = request.POST['current_path']
+            '''
             search_box = request.POST['search_box']
             category = request.POST['category']
             mode = request.POST.getlist('mode')
@@ -454,7 +455,7 @@ def data(request):
             label = request.POST.getlist('label')
             time_range = request.POST.getlist('time_range')
             bounding_box = request.POST.getlist('bounding_box')
-
+            '''
 
             #fs = FileSystemStorage(location=os.path.join(settings.CORE_DIR, 'data') + "/users")
             fs = FileSystemStorage(location="/home/" + request.user.get_username() + "/ag_data")
