75d399cd3751f372839ba4df25042a7acf343cbc
impanyu <impanyu>
2023-01-09 15:59:54 -0600

    create template search.html, display search result on this template

--- apps/static/assets/js/search.js
+++ apps/static/assets/js/search.js
@@ -388,7 +388,7 @@ function get_file_list(){
           items = data["items"];
           points = data["2d_points"]
           console.info(items)
-          draw_points(points);
+          draw_points(items);
 
 
           for(var i=0;i<items.length;i++){
