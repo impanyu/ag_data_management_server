d861f21890ca498bafa958af5f687420c80d71d3
impanyu <impanyu>
2023-01-09 22:18:58 -0600

    create template search.html, display search result on this template

--- apps/home/data.py
+++ apps/home/data.py
@@ -602,14 +602,16 @@ def top_down(dir_root, data_points):
                 data_points[dir_root][key] = meta_data[key]
 
     meta_data = data_points[dir_root]
-
+    '''
     for p in os.listdir(dir_root):
         for key in meta_data:
             data_points[p][key] = meta_data[key]
         top_down(dir_root+"/"+p,data_points)
-
+    '''
 
 def register_file_meta(file_path,data_points):
+    if file_path.split(".")[-1] == "meta":
+        return
     meta_data_file_path = file_path + ".meta"
     if os.path.exists(meta_data_file_path):
         with open(meta_data_file_path, "r") as meta_data_file:
