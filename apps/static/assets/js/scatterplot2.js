77e57b5871a0d9b01ab120e463a0d962ab0c33ab
impanyu <impanyu>
2023-01-09 22:28:47 -0600

    create template search.html, display search result on this template

--- apps/static/assets/js/scatterplot2.js
+++ apps/static/assets/js/scatterplot2.js
@@ -1,103 +1,107 @@
-height = d3.select("#map_main").node().getBoundingClientRect().height;
-width = d3.select("#map_main").node().getBoundingClientRect().width;
-k = height / width;
-
-const random = d3.randomNormal(0, 0.2);
-const sqrt3 = Math.sqrt(3);
-
 data = [].concat(
     Array.from({length: 300}, () => [random() + sqrt3, random() + 1, 0]),
     Array.from({length: 300}, () => [random() - sqrt3, random() + 1, 1]),
     Array.from({length: 300}, () => [random(), random() - 1, 2])
   );
 
-x = d3.scaleLinear()
-    .domain([-4.5, 4.5])
-    .range([0, width])
+function draw_2d_points(data){
 
-y = d3.scaleLinear()
-    .domain([-4.5 * k, 4.5 * k])
-    .range([height, 0])
+    height = d3.select("#map_main").node().getBoundingClientRect().height;
+    width = d3.select("#map_main").node().getBoundingClientRect().width;
+    k = height / width;
 
-z = d3.scaleOrdinal()
-    .domain(data.map(d => d[2]))
-    .range(d3.schemeCategory10)
-
-xAxis = (g, x) => g
-    .attr("transform", `translate(0,${height})`)
-    .call(d3.axisTop(x).ticks(12))
-    .call(g => g.select(".domain").attr("display", "none"))
-
-yAxis = (g, y) => g
-    .call(d3.axisRight(y).ticks(12 * k))
-    .call(g => g.select(".domain").attr("display", "none"))
+    const random = d3.randomNormal(0, 0.2);
+    const sqrt3 = Math.sqrt(3);
 
 
-grid = (g, x, y) => g
-    .attr("stroke", "currentColor")
-    .attr("stroke-opacity", 0.1)
-    .call(g => g
-      .selectAll(".x")
-      .data(x.ticks(12))
-      .join(
-        enter => enter.append("line").attr("class", "x").attr("y2", height),
-        update => update,
-        exit => exit.remove()
-      )
-        .attr("x1", d => 0.5 + x(d))
-        .attr("x2", d => 0.5 + x(d)))
-    .call(g => g
-      .selectAll(".y")
-      .data(y.ticks(12 * k))
-      .join(
-        enter => enter.append("line").attr("class", "y").attr("x2", width),
-        update => update,
-        exit => exit.remove()
-      )
-        .attr("y1", d => 0.5 + y(d))
-        .attr("y2", d => 0.5 + y(d)));
 
-const zoom = d3.zoom()
-      .scaleExtent([0.5, 32])
-      .on("zoom", zoomed);
+    x = d3.scaleLinear()
+        .domain([-4.5, 4.5])
+        .range([0, width])
 
-svg = d3.select("#files_plot")
-    .append("svg")
-     .attr("viewBox", [0, 0, width, height]);
+    y = d3.scaleLinear()
+        .domain([-4.5 * k, 4.5 * k])
+        .range([height, 0])
 
-  const gGrid = svg.append("g");
+    z = d3.scaleOrdinal()
+        .domain(data.map(d => d[2]))
+        .range(d3.schemeCategory10)
 
-/*
-  const gDot = svg.append("g")
-      .attr("fill", "none")
-      .attr("stroke-linecap", "round");
+    xAxis = (g, x) => g
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisTop(x).ticks(12))
+        .call(g => g.select(".domain").attr("display", "none"))
+
+    yAxis = (g, y) => g
+        .call(d3.axisRight(y).ticks(12 * k))
+        .call(g => g.select(".domain").attr("display", "none"))
 
 
-  gDot.selectAll("path")
-    .data(data)
-    .join("path")
-      .attr("d", d => `M${x(d[0])},${y(d[1])}h0`)
-      .attr("stroke", d => z(d[2]))
-      .attr("stroke-width", 20);
-*/
-    // Add dots
-    const gDot = svg.append('g')
-    .selectAll("dot")
-    .data(data)
-    .join("circle")
-        .attr("cx", function (d) { return x(d[0]); } )
-        .attr("cy", function (d) { return y(d[1]); } )
-        .attr("r", 10)
-        .attr("stroke","black")
-        .attr("stroke-width",2)
-        .style("fill", d => z(d[2]));
+    grid = (g, x, y) => g
+        .attr("stroke", "currentColor")
+        .attr("stroke-opacity", 0.1)
+        .call(g => g
+          .selectAll(".x")
+          .data(x.ticks(12))
+          .join(
+            enter => enter.append("line").attr("class", "x").attr("y2", height),
+            update => update,
+            exit => exit.remove()
+          )
+            .attr("x1", d => 0.5 + x(d))
+            .attr("x2", d => 0.5 + x(d)))
+        .call(g => g
+          .selectAll(".y")
+          .data(y.ticks(12 * k))
+          .join(
+            enter => enter.append("line").attr("class", "y").attr("x2", width),
+            update => update,
+            exit => exit.remove()
+          )
+            .attr("y1", d => 0.5 + y(d))
+            .attr("y2", d => 0.5 + y(d)));
 
-  const gx = svg.append("g");
+    const zoom = d3.zoom()
+          .scaleExtent([0.5, 32])
+          .on("zoom", zoomed);
 
-  const gy = svg.append("g");
+    svg = d3.select("#files_plot")
+        .append("svg")
+         .attr("viewBox", [0, 0, width, height]);
 
-  svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
+      const gGrid = svg.append("g");
 
+    /*
+      const gDot = svg.append("g")
+          .attr("fill", "none")
+          .attr("stroke-linecap", "round");
+
+
+      gDot.selectAll("path")
+        .data(data)
+        .join("path")
+          .attr("d", d => `M${x(d[0])},${y(d[1])}h0`)
+          .attr("stroke", d => z(d[2]))
+          .attr("stroke-width", 20);
+    */
+        // Add dots
+        const gDot = svg.append('g')
+        .selectAll("dot")
+        .data(data)
+        .join("circle")
+            .attr("cx", function (d) { return x(d[0]); } )
+            .attr("cy", function (d) { return y(d[1]); } )
+            .attr("r", 10)
+            .attr("stroke","black")
+            .attr("stroke-width",2)
+            .style("fill", d => z(d[2]));
+
+      const gx = svg.append("g");
+
+      const gy = svg.append("g");
+
+      svg.call(zoom).call(zoom.transform, d3.zoomIdentity);
+}
   function zoomed({transform}) {
     const zx = transform.rescaleX(x).interpolate(d3.interpolateRound);
     const zy = transform.rescaleY(y).interpolate(d3.interpolateRound);
@@ -108,3 +112,4 @@ svg = d3.select("#files_plot")
   }
 
 
+draw_2d_points(data);
