5ae2804ea71374a6a0438a71b579a53a11dee22a
impanyu <impanyu>
2023-02-15 18:07:27 -0600

    ui modification

--- apps/static/assets/js/filter_condition_display.js
+++ apps/static/assets/js/filter_condition_display.js
@@ -1,6 +1,6 @@
 function toggle_filter_condition(){
    if(document.querySelector("#filter-condition").style.display == "block")
      document.querySelector("#filter-condition").style.display = "none";
-    else:
+    else
      document.querySelector("#filter-condition").style.display = "block";
 }
