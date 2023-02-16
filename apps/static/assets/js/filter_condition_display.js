c667c52b1f63a7360b8b36ecd6a64e7636dd3253
impanyu <impanyu>
2023-02-15 18:04:45 -0600

    ui modification

---
+++ apps/static/assets/js/filter_condition_display.js
@@ -0,0 +1,6 @@
+function toggle_filter_condition(){
+   if(document.querySelector("#filter-condition").style.display == "block")
+     document.querySelector("#filter-condition").style.display = "none";
+    else:
+     document.querySelector("#filter-condition").style.display = "block";
+}
