9d1c01196dd3df1064d1702e7b1709e8cefea3a1
impanyu <impanyu>
2023-02-15 18:24:13 -0600

    ui modification

--- apps/static/assets/js/filter_condition_display.js
+++ apps/static/assets/js/filter_condition_display.js
@@ -6,8 +6,13 @@ function toggle_filter_condition(){
 }
 
 function toggle_filter_condition_2(){
-   if(document.querySelector("#filter-condition").style.height == "0px")
-     document.querySelector("#filter-condition").style.height = "500px";
-    else
+   if(document.querySelector("#filter-condition").style.height == "0px"){
+     document.querySelector("#filter-condition").style.height = "1080px";
+     document.querySelector("#filter-condition").style.padding = "1.25rem";
+
+     }
+    else{
      document.querySelector("#filter-condition").style.height = "0px";
+     document.querySelector("#filter-condition").style.padding = "0rem";
+     }
 }
