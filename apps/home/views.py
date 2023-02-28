31dd502d82300df76d370140b3679e60acd76d2a
impanyu <impanyu>
2023-02-27 21:06:24 -0600

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -377,18 +377,17 @@ def data(request):
                     parent_meta_data_file_name = "_".join(parent_dir.split("/")[1:]) + ".json"
                     parent_meta_data_file_path = os.path.join(settings.CORE_DIR, 'data', parent_meta_data_file_name)
 
-                    with open(parent_meta_data_file_path, "r+") as parent_meta_data_file:
-                        parent_meta_data_file.seek(0)
+                    if not os.path.exists(parent_meta_data_file_path):
+                        with open(parent_meta_data_file_path, "w") as parent_meta_data_file:
+                            json.dump({"subdirs":[]}, parent_meta_data_file)
+
+                    with open(parent_meta_data_file_path, "r") as parent_meta_data_file:
                         parent_meta_data = json.load(parent_meta_data_file)
-                    '''
-                        if "subdirs" in parent_meta_data:
-                            parent_meta_data["subdirs"].append(root_abs_path)
-                        else:
-                            parent_meta_data["subdirs"] = [root_abs_path]
-                    
+                        parent_meta_data["subdirs"].append(root_abs_path)
+
                     with open(parent_meta_data_file_path, "w") as parent_meta_data_file:
                         json.dump(parent_meta_data, parent_meta_data_file)
-                    '''
+
                 
                     #adjust_meta_data(parent_dir)
 
