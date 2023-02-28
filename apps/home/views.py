9f1ee937c69c284f9ccb26987da8b00d4931e3a8
impanyu <impanyu>
2023-02-27 20:41:51 -0600

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -386,7 +386,7 @@ def data(request):
                     with open(parent_meta_data_file_path, "w") as parent_meta_data_file:
                         json.dump(parent_meta_data, parent_meta_data_file)
                 
-                    adjust_meta_data(parent_dir)
+                    #adjust_meta_data(parent_dir)
 
 
                 '''
