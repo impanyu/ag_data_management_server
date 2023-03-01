86aebe5555b444d09735eb075c96d90650bc9407
impanyu <impanyu>
2023-02-28 20:32:40 -0600

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -928,6 +928,7 @@ def search(root_dir, search_box, category, mode, format, label, time_range, spat
     # search data
     # if we need to do a full search
     if "File" in mode or "Folder" in mode:
+        print("mode "+str(mode))
         root_dir = root_dir + "/ag_data"
         meta_data_file_name = "_".join(root_dir.split("/")[1:]) + ".json"
         with open(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name), "r") as meta_data_file:
