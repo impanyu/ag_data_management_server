4fe9cd9fd1bdfe078ed23b6a40c71fdff8044b3b
impanyu <impanyu>
2023-03-01 18:42:06 -0600

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -554,7 +554,7 @@ def filtering_condition(meta_data, search_box, category, mode, format, label, ti
             print("not has time")
             return False
 
-    '''
+
     southwest = bounding_box[0]
     northeast = bounding_box[1]
 
@@ -562,7 +562,7 @@ def filtering_condition(meta_data, search_box, category, mode, format, label, ti
     item_northeast_lng = meta_data["spatial_range"]["northeast"]["lng"]
     item_southwest_lat = meta_data["spatial_range"]["southwest"]["lat"]
     item_southwest_lng = meta_data["spatial_range"]["southwest"]["lng"]
-
+    '''
     if not (southwest == "Southwest Corner" or northeast == "Northeast Corner"):
         lower_lat, upper_lat, left_ln, right_ln = extract_coordinates(southwest, northeast)
         if not overlap(lower_lat, upper_lat, item_southwest_lat, item_northeast_lat):
