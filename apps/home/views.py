08bcd440b9f5bde04df58943a93ef95434818089
impanyu <impanyu>
2023-03-02 19:30:18 -0600

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -660,7 +660,8 @@ def data(request):
 
         elif load_template == 'meta_data':
             current_path = request.POST['current_path']
-            meta_data = get_meta_data("/home/"+current_path)
+            meta_data={"m":1}
+            #meta_data = get_meta_data("/home/"+current_path)
             response = json.dumps(meta_data)
             return HttpResponse(response)
 
