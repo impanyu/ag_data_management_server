4deefb5b6e17fa8250842b12f7fae3505d1b3f40
impanyu <impanyu>
2023-03-02 22:34:59 -0600

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -632,8 +632,8 @@ function get_meta_data(){
                   '<label class="form-check-label" for="'+meta_key+'" ><b>'+meta_key.toUpperCase()+'</b></label>'+
                   '</div>'+
                   '<div class="col-lg-6 col-6">';
-                  for (meta_value_element in meta_value){
-                     meta_html += meta_value_element+
+                  for (i in meta_value){
+                     meta_html += meta_value[i]+
                      '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="remove_meta_data_option()">'+
                      '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'+
                      '</svg>' + ','
@@ -645,11 +645,11 @@ function get_meta_data(){
                                   '<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Add'+
                                   '<span class="caret"></span></button>'+
                                   '<ul class="dropdown-menu">';
-                                 for(option in meta_data_options[meta_key]){
-                                   meta_html += '<li onclick="add_meta_data_option()">'+ +'</li>';
+                                 for(j in meta_data_options[meta_key]){
+                                   meta_html += '<li onclick="add_meta_data_option()">'+ meta_data_options[meta_key][i] +'</li>';
                                    }
 
-                                 meta_html+= '</ul></div>';
+                                 meta_html+= '</ul></div></div>';
 
                      //'<input class="form-control" type="text" id="meta_key" value="'+meta_value+'">'+
 
