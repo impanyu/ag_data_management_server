664caf198c898974bef2f48e0859aa6de19a7b7c
impanyu <impanyu>
2023-03-03 17:16:33 -0600

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -641,7 +641,7 @@ function get_meta_data(){
                               meta_html += '<div class="border shadow p-3 bg-white rouded d-flex align-items-center" style = "background-color:white; height:2.8rem; width:80%; padding-left:1rem; padding-right:1rem">'
                               meta_html += " ";
                               for (i in meta_value){
-                                 meta_html += '<div>'+meta_value[i]+
+                                 meta_html += '<div class ="meta_option">'+meta_value[i]+
                                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="remove_meta_data_option()">'+
                                  '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'+
                                  '</svg>' + ',' +'</div>'
