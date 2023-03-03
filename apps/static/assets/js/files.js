a5e4566a5cc25f53d684a77ac6ab8d42a83ba32c
impanyu <impanyu>
2023-03-03 16:31:20 -0600

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -633,7 +633,7 @@ function get_meta_data(){
 
 
              meta_html += '</div>';
-             meta_html += '<div class="dropdown col-lg-1 col-1">'+
+             meta_html += '<div class="dropdown col-lg-2 col-2">'+
                                   '<button class="btn btn-info dropdown-toggle" type="button" data-toggle="dropdown">Add'+
                                   '<span class="caret"></span></button>'+
                                   '<ul class="dropdown-menu">';
