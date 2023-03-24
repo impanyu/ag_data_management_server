e4e03df8c16e4ff5a6d01fc13145aa8ea51b5d6b
impanyu <impanyu>
2023-03-24 02:27:31 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -688,7 +688,7 @@ function  add_meta_data_option(self){
    if (current_labels.has(new_label))
       return;
 
-   label_html = '<div class ="meta_option">'+new_label+'<span>'+meta_value[i]+'</span>'+
+   label_html = '<div class ="meta_option">'+'<span>'+new_label+'</span>'+
                                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="remove_meta_data_option()">'+
                                  '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'+
                                  '</svg>' + ',' +'</div>';
