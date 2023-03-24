88b7d2f4ba76be5ea03bc45278e202666f9386ce
impanyu <impanyu>
2023-03-24 01:49:25 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -645,7 +645,7 @@ function get_meta_data(){
                                  label_html += '<div class ="meta_option">'+meta_value[i]+
                                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="remove_meta_data_option()">'+
                                  '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'+
-                                 '</svg>' + ',' +'</div>'
+                                 '</svg>' + ',' +'</div>';
                               }
                              document.querySelector("#label").innerHTML = label_html;
 
@@ -689,7 +689,7 @@ function  add_meta_data_option(event){
    label_html = '<div class ="meta_option">'+new_label+
                                  '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16" onclick="remove_meta_data_option()">'+
                                  '<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>'+
-                                 '</svg>' + ',' +'</div>'
+                                 '</svg>' + ',' +'</div>';
    document.querySelector("#label").innerHTML =  document.querySelector("#label").innerHTML+label_html;
    labels.append(new_label);
 }
