114ab9306d5aadc92ac777ab52c9ad10703794cc
impanyu <impanyu>
2023-03-28 18:52:02 -0500

    ui modification

--- apps/static/assets/js/custom.js
+++ apps/static/assets/js/custom.js
@@ -3,8 +3,9 @@
 nav_links = document.querySelectorAll('.nav-link');
 
 for(i=0;i<nav_links.length;i++){
-  nav_links[i].addEventListener('mouseover', function(this){
-     add_active(this);
+
+  nav_links[i].addEventListener('mouseover', function(event){
+     add_active(event);
 });
   nav_links[i].addEventListener('mouseout', function(this) {
   remove_active(this);
@@ -14,8 +15,8 @@ for(i=0;i<nav_links.length;i++){
 
 
 
-function add_active(self){
-self.classList.add('active');
+function add_active(event){
+event.target.classList.add('active');
 }
 
 function remove_active(self){
