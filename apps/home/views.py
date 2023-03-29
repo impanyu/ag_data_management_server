f499b9659b6b0cc35bad271270b1363f0b4260a4
impanyu <impanyu>
2023-03-29 10:53:47 -0500

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -769,7 +769,7 @@ def data(request):
 
             response["items"] += public_items
             '''
-            #response["2d_points"] = dim_reduction(response["items"]).tolist()
+            response["2d_points"] = dim_reduction(response["items"]).tolist()
 
 
 
