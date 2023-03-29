17876f7e881ae07888039f611d23d59e8839f5ba
impanyu <impanyu>
2023-03-29 10:43:26 -0500

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -1055,8 +1055,8 @@ def search(root_dir, search_box, category, mode, format, label, time_range, spat
     meta_data_file_name = "_".join(root_dir.split("/")[1:]) + ".json"
     with open(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name), "r") as meta_data_file:
         meta_data = json.load(meta_data_file)
-        if filtering_condition(meta_data, search_box, category, mode, format, label, time_range, spatial_range):
-            result.append(meta_data)
+        #if filtering_condition(meta_data, search_box, category, mode, format, label, time_range, spatial_range):
+         #   result.append(meta_data)
         for subdir in meta_data["subdirs"]:
             sub_result = search(subdir, search_box, category, mode, format, label, time_range, spatial_range)
             result += sub_result
--- apps/home/views.py
+++ apps/home/views.py
@@ -757,7 +757,7 @@ def data(request):
             # search the user's own items
             if "My Own Data" in privilege:
                 root_dir = os.path.join("/home",request.user.get_username(),"ag_data")
-                #response["items"] = search(root_dir,search_box,category,mode,format,label,time_range,spatial_range)
+                response["items"] = search(root_dir,search_box,category,mode,format,label,time_range,spatial_range)
             #if "Domain" in mode:
                 #search_domains()
                 print(response["items"])
