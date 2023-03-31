7249f6011b529181338672aa1ceb10eb31567ba9
impanyu <impanyu>
2023-03-31 16:43:21 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -546,7 +546,7 @@ else if (suffix == "tif" || suffix == "tiff" || suffix == "png" || suffix == "jp
 
                           };
 
-                        new_center = new google.maps.Latlng((north+south)/2,(east+west)/2);
+                        new_center = new google.maps.LatLng((north+south)/2,(east+west)/2);
                         map_main.setCenter(new_center);
                         map_main.setZoom(15);
 
