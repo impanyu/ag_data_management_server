af9c31e8a303c27542dfe360611db4e020529a8e
impanyu <impanyu>
2023-04-05 07:59:14 -0500

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -944,7 +944,7 @@ def update_meta(file_path,new_meta_data):
             #meta_data[key]={"start": "01/01/2030 00:00:00", "end": "01/01/2030 00:00:00"}
             meta_data[key]["start"] = datetime.strptime(new_meta_data["time_range"]["start"], "%m/%d/%Y").strftime("%m/%d/%Y %H:%M:%S")
             meta_data[key]["end"] = datetime.strptime(new_meta_data["time_range"]["end"], "%m/%d/%Y").strftime("%m/%d/%Y %H:%M:%S")
-        '''
+
         elif key == "other_meta":
             #other_meta = new_meta_data[key].replace("\n",",")
             #other_meta = json.loads("{" + other_meta + "}")
@@ -962,7 +962,7 @@ def update_meta(file_path,new_meta_data):
 
                 meta_data[k] = v
 
-
+        '''
         elif key == "spatial_range":
             if new_meta_data[key]["southwest"] == "southwest":
                 continue
