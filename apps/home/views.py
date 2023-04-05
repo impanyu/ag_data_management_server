2ae405e4441ef6ce95a5ed8a0378825784ecf0e9
impanyu <impanyu>
2023-04-05 01:54:38 -0500

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -311,6 +311,7 @@ def data(request):
                 new_path = abs_path +"_"+str(i)
                 i = i+1
             os.makedirs(new_path)
+            aggregate_meta_data(new_path)
 
             return HttpResponse("folder creation complete!")
 
