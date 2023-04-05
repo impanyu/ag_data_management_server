cd642d107cfc0e0b80f3c889d2f77e392e93220e
impanyu <impanyu>
2023-04-05 07:22:20 -0500

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -917,7 +917,8 @@ def update_meta(file_path,new_meta_data):
     meta_data_file_name = "_".join(file_path.split("/")[1:]) + ".json"
     #with open(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name), "r") as meta_data_file:
     #    meta_data = json.load(meta_data_file)
-    meta_data ={"public" : "True"}
+    #meta_data ={"public" : "True"}
+    meta_data = {}
 
     print(new_meta_data)
     for key in new_meta_data:
