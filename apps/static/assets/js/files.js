30e89c5468bd0888e399ee68286a6153b9e3cac7
impanyu <impanyu>
2023-04-04 23:37:17 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -505,7 +505,7 @@ function update_file(){
         },
         contentType: "text/plain",
         success: function(data, status){
-        console.info(data)
+        console.info(data);
           alert("file updated");
 
   }});
