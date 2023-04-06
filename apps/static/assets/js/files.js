2052d99d19b630716572b47908cc534a87cd020e
impanyu <impanyu>
2023-04-05 23:39:31 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -643,15 +643,15 @@ function set_tool_panel(){
                          '<div class="col-lg-7 col-6">'+
                              '<input class="form-control"   type="text" value="Specify Arguments: '+arg +' Here" id="'+arg+'" disabled>'+
                         '</div>'+
-                          <div class="col-lg-2 col-6">
-                                      <div href="#" class="btn btn-lg btn-success"   id="remove_arg"  onclick="remove_tool_arg()">
-                                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                        <!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
-                                        <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
-                                    </svg>
+                          '<div class="col-lg-2 col-6">'+
+                                    '<div href="#" class="btn btn-lg btn-success"   id="remove_arg"  onclick="remove_tool_arg()">'  +
+                                      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">' +
 
-                               </div>
-                          </div>
+                                      ' <path d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>' +
+                                   ' </svg>'+
+
+                             '</div>'  +
+                         '</div>' +
 
                         '</div>'
           if(arg.indexOf("file") !=-1 || arg.indexOf("dir") !=-1)
