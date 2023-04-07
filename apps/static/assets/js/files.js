d4fb3bec9c84e8b5070e6403c7fc0ebcc42a252f
impanyu <impanyu>
2023-04-06 20:39:40 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -783,6 +783,7 @@ function add_tool_arg(){
 
                         '</div>'
          if(arg_type == "File" || arg_type == "Directory"){
+               console.info(document.querySelector("#arg_"+arg_name));
                document.querySelector("#arg_"+arg_name).addEventListener("click",function(){display_file_selection(arg_name,user);});
                //document.querySelector("#arg_"+arg_name).disabled = true;
          }
