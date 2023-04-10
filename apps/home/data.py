d3f07971c96b9ed98fcfa2501fc6735c5bf3417b
impanyu <impanyu>
2023-04-09 19:21:51 -0500

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -1448,6 +1448,7 @@ def run_tool(entry_point,arg_values, arg_types,user):
     output = client.containers.run(
         image_name,
         #command=[main_cmd, script_path] + [arg_values[arg_name] for arg_name in arg_values],
+        command=[main_cmd, script_path],
         volumes={f"/data/{user}": {"bind": f"/{user}", "mode": "rw"}},
         #working_dir=working_dir,
         #environment={"VAR1": "value1", "VAR2": "value2"},
