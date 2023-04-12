43a8ed0d4d04d32a0fe2fffe82adbfaba3606e2b
impanyu <impanyu>
2023-04-12 14:53:30 -0500

    ui modification

---
+++ apps/static/assets/js/domains.js
@@ -0,0 +1,43 @@
+function create_domain(){
+
+
+
+
+}
+
+
+function set_create_domain_overlay(){
+        const create_domain_overlay = document.querySelector('#create_domain_overlay');
+
+        create_domain_overlay.addEventListener('click', function(event) {
+            this.style.display  = "none";
+        });
+
+        const create_domain_tab = document.querySelector('#create_domain_tab');
+
+        create_domain_tab.addEventListener('click', function(event) {
+           event.stopPropagation();
+        });
+
+
+        const create_domain_button = document.querySelector('#create_domain_button');
+
+        create_domain_button.addEventListener('click', function(event) {
+
+           new_domain_name = document.querySelector("#new_domain_name").value;
+           create_domain(new_domain_name);
+        });
+
+
+        const cancel_create_domain_button = document.querySelector('#cancel_create_domain_button');
+
+        cancel_create_domain_button.addEventListener('click', function(event) {
+           create_domain_overlay.style.display  = "none";
+        });
+
+        const create_domain_li = document.querySelector('#create_domain_li');
+
+        create_domain_li.addEventListener('click', function(event) {
+           create_domain_overlay.style.display  = "flex";
+        });
+}
