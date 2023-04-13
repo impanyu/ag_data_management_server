dc0c0ed0de68a169cc0e7e845a3154fc58342e5e
impanyu <impanyu>
2023-04-13 15:32:03 -0500

    ui modification

--- apps/home/data.py
+++ apps/home/data.py
@@ -1172,7 +1172,7 @@ def search(root_dir, search_box, category, mode, format, label, realtime, time_r
 
     # root_dir = root_dir + "/ag_data"
     meta_data_file_name = "_".join(root_dir.split("/")[1:]) + ".json"
-    if not os.path.exists(open(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name), "r")):
+    if not os.path.exists(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name)):
         return [os.path.join(settings.CORE_DIR, 'data', meta_data_file_name)]
     with open(os.path.join(settings.CORE_DIR, 'data', meta_data_file_name), "r") as meta_data_file:
         meta_data = json.load(meta_data_file)
