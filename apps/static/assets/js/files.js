173dfc2d28e6ebab0c7447f7620ae6bbeeaef9c9
impanyu <impanyu>
2023-04-14 07:52:06 -0500

    ui modification

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -983,6 +983,7 @@ function select_file(arg_name,path){
     sub_dir_name = path.split("/")[path.split("/").length-1];
 
     i = select_file_names.indexOf(sub_dir_name);
+    console.info("here");
 
     document.querySelector("#file_item_"+i).style.backgroundColor = "#69cfff";
     //if(previous_selected_file && previous_selected_file!= document.querySelector("#file_item_"+i)
