5dd963a6dce5fa9c1a2afbbf95bd5cb00791aad1
impanyu <impanyu>
2023-04-14 14:16:52 -0500

    ui modification

--- apps/static/assets/js/scatterplot2.js
+++ apps/static/assets/js/scatterplot2.js
@@ -149,7 +149,7 @@ function draw_2d_points(data){
     .style('fill', colorScale);
 
   legend.append('text')
-    .attr('x', legendX - legendSpacing)
+    .attr('x', legendX + legendSpacing)
     .attr('y', legendY + legendSize / 2)
     .attr('dy', '.35em')
     .style('text-anchor', 'end')
