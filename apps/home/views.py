6c7130d1b4635e38d39d4f40b5b5e7bc4e5f0109
impanyu <impanyu>
2023-04-14 15:31:13 -0500

    ui modification

--- apps/home/views.py
+++ apps/home/views.py
@@ -74,6 +74,7 @@ def pages(request):
         elif load_template == 'collections.html':
             context['current_path'] = request.GET['current_path']
             context['segment'] = load_template
+
             #context = {'segment': 'index'}
             #print("in domains")
             #domains = get_domains()
@@ -122,12 +123,14 @@ def pages(request):
 
 
 
+
             html_template = loader.get_template('home/files.html')
             return HttpResponse(html_template.render(context, request))
 
         elif load_template == "collection.html":
             context['current_path'] = request.GET['current_path']
             context['segment'] = load_template
+
 
 
 
@@ -152,6 +155,8 @@ def pages(request):
             return HttpResponse(html_template.render(context, request))
 
         context['segment'] = load_template
+        global username
+        username = request.user.get_username()
 
         html_template = loader.get_template('home/' + load_template)
         return HttpResponse(html_template.render(context, request))
