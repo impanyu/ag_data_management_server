a3d3208294fa00889199b9eec7515a32f1e3a3e1
impanyu <impanyu>
2023-04-17 17:23:44 -0500

    ui modification

--- apps/home/file_monitor.py
+++ apps/home/file_monitor.py
@@ -13,14 +13,16 @@ class EventHandler(pyinotify.ProcessEvent):
         self.container_id = container_id
 
     def process_IN_ACCESS(self, event):
-        if self.find_container_id_by_pid(event.pid) == self.get_pid(event):
+        pid = self.get_pid(event)
+        if self.find_container_id_by_pid(pid) == self.container_id:
             self.accessed_files.add(event.pathname)
 
         #print(f"File {event.pathname} was read by PID {event.pid}")
 
     def process_IN_CREATE(self, event):
         #if not event.dir:
-        if self.find_container_id_by_pid(event.pid) == self.get_pid(event):
+        pid = self.get_pid(event)
+        if self.find_container_id_by_pid(pid) == self.container_id:
             self.created_files.add(event.pathname)
         #print(f"File {event.pathname} was created by PID {event.pid}")
 
@@ -30,7 +32,8 @@ class EventHandler(pyinotify.ProcessEvent):
 
     def process_IN_MODIFY(self, event):
         #if not event.dir:
-        if self.find_container_id_by_pid(event.pid) == self.get_pid(event):
+        pid = self.get_pid(event)
+        if self.find_container_id_by_pid(pid) == self.container_id:
             self.written_files.add(event.pathname)
             #print(f"File {event.pathname} was modified by PID {event.pid}")
 
