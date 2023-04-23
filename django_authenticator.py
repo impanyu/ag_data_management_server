fd5f9e9f22122be90c4db9f83c2b8d5de3aae412
impanyu <impanyu>
2023-04-22 22:04:19 -0500

    ui modification

--- django_authenticator.py
+++ django_authenticator.py
@@ -2,6 +2,11 @@ import requests
 from jupyterhub.auth import Authenticator
 from tornado import gen
 
+import requests
+from bs4 import BeautifulSoup
+
+
+
 class DjangoAuthenticator(Authenticator):
 
     @gen.coroutine
@@ -12,7 +17,18 @@ class DjangoAuthenticator(Authenticator):
         # URL of your Django application's authentication endpoint
         auth_url = "http://unlagdatamanagement.hopto.org/api/authenticate/"
 
-        response = requests.post(auth_url, data={"username": username, "password": password})
+        # Replace with the URL of your Django login page
+        login_url = "http://localhost/login/"
+
+        # Make a GET request to the login page to get the CSRF token
+        response = requests.get(login_url)
+        soup = BeautifulSoup(response.content, "html.parser")
+        csrf_token = soup.find("input", {"name": "csrfmiddlewaretoken"})["value"]
+
+        #csrf_token = 'token_value'
+        headers = {'X-CSRFToken': csrf_token}
+
+        response = requests.post(auth_url, headers = headers, data={"username": username, "password": password})
         print(response.status_code)
 
         if response.status_code == 200:
