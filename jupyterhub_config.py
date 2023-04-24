e14783efc214572029a78a651c3f16a68db35149
impanyu <impanyu>
2023-04-24 11:40:52 -0500

    ui modification

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -861,6 +861,10 @@ c.DockerSpawner.user = "root"
 
 # Enable users to access the Docker host
 c.DockerSpawner.network_name = "bridge"
+c.DockerSpawner.extra_create_kwargs.update({
+    'user': '0:0'
+})
+
 #c.DockerSpawner.extra_host_config = {'network_mode': 'bridge'}
 #c.DockerSpawner.extra_host_config = {"network_mode": "host"}
 #c.DockerSpawner.port_range = (10000, 11000)
