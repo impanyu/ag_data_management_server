80d3d423f52970fca66f33d8bcc7bca02e0102f1
impanyu <impanyu>
2023-04-26 17:48:35 -0500

    ui modification

--- apps/home/api.py
+++ apps/home/api.py
@@ -38,11 +38,38 @@ def data(request):
             api_keys = get_api_keys()
             meta_data = get_meta_data("/data" + current_path)
             if api_key in api_keys and (user in api_keys[api_key] or meta_data["public"] == "True"):
-                meta_data = get_meta_data("/data" + current_path)
+                pass
             else:
                 meta_data = "API key is not valid!"
             response = json.dumps(meta_data)
             return HttpResponse(response)
+
+        elif load_template == 'api_list_sub_items':
+            current_path = request.GET['current_path']
+            api_key = request.GET['key']
+            user = current_path.split("/")[1]
+            items = []
+            api_keys = get_api_keys()
+            meta_data = get_meta_data("/data" + current_path)
+            if api_key in api_keys and (user in api_keys[api_key] or meta_data["public"] == "True"):
+                for sub_path in meta_data["subdirs"]:
+
+                    sub_meta_data = get_meta_data(sub_path)
+                    if "Tool" in sub_meta_data["mode"]:
+                        running_containers = get_running_containers(sub_path)
+                        if len(running_containers) == 0:
+                            sub_meta_data["running"] = "False"
+                        else:
+                            sub_meta_data["running"] = "True"
+                    items.append(sub_meta_data)
+                items = sorted(items, key=lambda item: item["name"])
+
+            else:
+                items = "API key is not valid!"
+            response = json.dumps(items)
+            return HttpResponse(response)
+
+
 
     except template.TemplateDoesNotExist:
 
