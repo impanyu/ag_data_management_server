9f3d6abda07392e100f93e3fe90cf5965935677c
impanyu <impanyu@gmail.com>
2023-07-13 17:52:05 -0500

    stock update

--- apps/home/views.py
+++ apps/home/views.py
@@ -110,15 +110,9 @@ def pages(request):
                 except FileNotFoundError:
                     print("The file does not exist.")
 
-            my_dict = {
-                'key1': 'value1',
-                'key2': 'value2',
-                'profit': 'value3'
-                # add as many keys and values as you need
-            }
 
-            context['stock'] = stock_data[0]
-            context['stock1'] = 1
+            context['stock'] = stock_data
+      
             #context = {'segment': 'index'}
             #print("in domains")
             #domains = get_domains()
