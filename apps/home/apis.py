9e31b82e57128594fbf8a66e38a2e642744077d9
impanyu <impanyu@gmail.com>
2024-03-13 17:06:17 -0500

    test

--- apps/home/apis.py
+++ apps/home/apis.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 class API(APIView):
