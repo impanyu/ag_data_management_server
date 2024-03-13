c5a012b75894b1cb4603a49ed0ef18d6f1c6a130
impanyu <impanyu@gmail.com>
2024-03-13 12:34:25 -0500

    add api test

--- core/urls.py
+++ core/urls.py
@@ -9,11 +9,32 @@ from django.urls import path, re_path,include  # add this
 from django.conf.urls.static import static
 from django.conf import settings
 
+
+from apps.home import api
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path('accounts/', include('allauth.urls')),
     path("", include("apps.authentication.urls")), # Auth routes - login / register
     path("", include("apps.home.urls")),             # UI Kits Html files
+    path('api-test/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 ]
 
