1db9c3f32d0fc1215ede712699278e090cd49e74
impanyu <impanyu@gmail.com>
2024-03-27 19:40:19 -0500

    test

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -36,9 +36,9 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),#,
     path('api/upload/', FileUploadView.as_view(), name='file-upload'),
-    path('api/download/', FileDownloadView.as_view(), name='file-upload'),
-    path('api/meta_data/', GetMetaDataView.as_view(), name='file-upload'),
-    path('api/list_files/', ListFilesView.as_view(), name='file-upload'),
+    path('api/download/', FileDownloadView.as_view(), name='file-download'),
+    path('api/meta_data/', GetMetaDataView.as_view(), name='meta-data'),
+    path('api/list_files/', ListFilesView.as_view(), name='list-files'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     # The home page
     path('', views.index, name='home'),
