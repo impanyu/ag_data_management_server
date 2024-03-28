b9beeb60978d72274ce13d4f991636b095616afb
impanyu <impanyu@gmail.com>
2024-03-27 19:26:38 -0500

    add get meta data api

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),#,
     path('api/upload/', FileUploadView.as_view(), name='file-upload'),
     path('api/download/', FileDownloadView.as_view(), name='file-upload'),
-    path('api/metadata/', FileDownloadView.as_view(), name='file-upload'),
+    path('api/metadata/', GetMetaDataView.as_view(), name='file-upload'),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     # The home page
     path('', views.index, name='home'),
