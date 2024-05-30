7bfbd0d2bfd22780fb79b7072e067c9060bfc2c1
impanyu <impanyu@gmail.com>
2024-05-29 20:44:59 -0500

    test

--- apps/home/native_tools.py
+++ apps/home/native_tools.py
@@ -149,7 +149,7 @@ def populate_JD_dir(file_path,token):
 
                 #get the headers
                 headers = files_response.headers
-                print(files_response.status_code)
+                print("status code is"+files_response.status_code)
                 if files_response.status_code == 307:
                     file_location =  headers['Location']
                     #download the file
