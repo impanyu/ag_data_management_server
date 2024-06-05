9b79fbbbf1c3cf2915500b52d241267b8d8d1e04
impanyu <impanyu@gmail.com>
2024-06-05 17:02:45 -0500

    test

--- core/wsgi.py
+++ core/wsgi.py
@@ -5,7 +5,19 @@ Copyright (c) 2019 - present AppSeed.us
 
 import os
 
+
 from django.core.wsgi import get_wsgi_application
+
+import sys
+import logging
+
+
+# Redirect stdout and stderr to log files
+sys.stdout = open('/var/log/ag_data_management/debug.log', 'a+')
+sys.stderr = open('/var/log/ag_data_management/debug.log', 'a+')
+
+
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
