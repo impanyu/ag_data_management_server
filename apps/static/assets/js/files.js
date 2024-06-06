d989f22fbe0000a6650cc8c91139a42f68717b9d
impanyu <impanyu@gmail.com>
2024-06-06 17:11:00 -0500

    test

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -1667,6 +1667,7 @@ else if (suffix == "shp"){
                 success: function(response,status,xhr) {
                   x=xhr;
                   document.querySelector("#file_content").style.display="block";
+                  console.info(response);
                   const contentType = xhr.getResponseHeader('Content-Type');
                    // Extract the filename from the Content-Disposition header
                    //const filename =xhr.getResponseHeader('Content-Disposition').split('filename=')[1];
