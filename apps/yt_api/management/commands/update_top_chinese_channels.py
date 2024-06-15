11478342033f5a32f9ef361a65770312239cfdca
impanyu <impanyu@gmail.com>
2024-06-15 13:04:11 -0500

    test

--- apps/yt_api/management/commands/update_top_chinese_channels.py
+++ apps/yt_api/management/commands/update_top_chinese_channels.py
@@ -1,7 +1,7 @@
 import os
 import requests
 from django.core.management.base import BaseCommand
-from yt_api.models import YouTubeChannel
+from apps.yt_api.models import YouTubeChannel
 from django.utils import timezone
 import time
 
