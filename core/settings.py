39b39d9f61fbe9a29a52db9c4d4a89e7bd54a762
impanyu <impanyu@gmail.com>
2024-06-15 12:57:10 -0500

    test

--- core/settings.py
+++ core/settings.py
@@ -152,6 +152,16 @@ CELERY_BEAT_SCHEDULE = {
     }
 }
 
+# settings.py
+
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
