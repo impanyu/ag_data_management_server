6511dba45fc121cdda611996a472d92c7691a8ec
impanyu <impanyu@gmail.com>
2024-06-16 18:09:11 -0500

    test

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_BEAT_SCHEDULE = {
     'update-top-chinese-channels': {
         'task': 'apps.yt_api.tasks.update_top_chinese_channels_task',
-        'schedule': 20.0,  # every 1 day
+        'schedule': 36000,  # every 1 day
     }
 }
 CELERY_WORKER_CONCURRENCY = 5
