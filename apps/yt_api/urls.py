c8286fcc7e8a082c41d9623386ecef1dac39ba70
impanyu <impanyu@gmail.com>
2024-06-16 17:11:24 -0500

    test

--- apps/yt_api/urls.py
+++ apps/yt_api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path,re_path
 from .views import *
 
 urlpatterns = [
-    path('top-chinese-channels-subscribers/', YouTubeTopChineseChannelListSubscribers.as_view(), name='top-chinese-channels-subscribers'),
+    path('top-chinese-channels-subscribers/', YouTubeTopChineseChannelSubscribers.as_view(), name='top-chinese-channels-subscribers'),
     path('top-chinese-channels/', YouTubeTopChineseChannelList.as_view(), name='top-chinese-channels'),
     path('get-hello/', GetHello.as_view(), name='get-hello'),
     # Matches any html file
