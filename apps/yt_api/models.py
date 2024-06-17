c7a178411024c24855558146291168be770d809b
impanyu <impanyu@gmail.com>
2024-06-17 18:52:12 -0500

    test

--- apps/yt_api/models.py
+++ apps/yt_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+class YouTubeChannel(models.Model):
+    channel_id = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    icon_url = models.URLField(max_length=255, blank=True, null=True)
+    join_date = models.DateTimeField(blank=True, null=True)  # Add join_date field
+    location = models.CharField(max_length=255, blank=True, null=True)  # Add location field
 
+    def __str__(self):
+        return self.title
+
+
+
+class YouTubeChannelSubscribers(models.Model):
+    channel_id = models.CharField(max_length=255)   
+    subscribers = models.BigIntegerField()
+    last_updated = models.DateTimeField()
+
+    def __str__(self):
+        return self.channel_id
