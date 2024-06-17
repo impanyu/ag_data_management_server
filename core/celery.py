0e10d12274069b3f0f5e1bfe0aace37dd9d2924e
impanyu <impanyu@gmail.com>
2024-06-17 17:44:19 -0500

    test

--- core/celery.py
+++ core/celery.py
@@ -12,6 +12,12 @@ sys.stderr = open('/var/log/ag_data_management/debug.log', 'a+')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 # Print to verify the concurrency setting
 print(f"CELERY_WORKER_CONCURRENCY: {settings.CELERY_WORKER_CONCURRENCY}")
+# Print environment variables for debugging
+print(f"CELERY_BROKER_URL: {os.environ.get('CELERY_BROKER_URL')}")
+print(f"CELERY_RESULT_BACKEND: {os.environ.get('CELERY_RESULT_BACKEND')}")
+print(f"CELERY_WORKER_CONCURRENCY: {os.environ.get('CELERY_WORKER_CONCURRENCY')}")
+
+
 
 app = Celery('core')
 app.config_from_object('django.conf:settings', namespace='CELERY')
