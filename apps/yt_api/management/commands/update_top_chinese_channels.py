9aacd0c7b8ab15cc01ba2b1d1d9e685a086e4407
impanyu <impanyu@gmail.com>
2024-06-17 13:54:11 -0500

    test

--- apps/yt_api/management/commands/update_top_chinese_channels.py
+++ apps/yt_api/management/commands/update_top_chinese_channels.py
@@ -12,11 +12,11 @@ from apps.yt_api.utils import *
 
 
 class Command(BaseCommand):
-    help = 'Update top YouTube channel list'
+    help = 'Update top YouTube channel list and subs'
 
     def handle(self, *args, **kwargs):
         channel_ids = get_channel_ids()
-        #self.get_channel_updates_initial(channel_ids)
+        self.get_channel_updates_initial(channel_ids)
         self.get_channel_updates(channel_ids)
 
     '''   
--- apps/yt_api/models.py
+++ apps/yt_api/models.py
@@ -5,7 +5,7 @@ class YouTubeChannel(models.Model):
     channel_id = models.CharField(max_length=255, unique=True)
     title = models.CharField(max_length=255)
     description = models.TextField()
-    icon_url = models.URLField(max_length=200, blank=True, null=True)
+    icon_url = models.URLField(max_length=255, blank=True, null=True)
     join_date = models.DateTimeField(blank=True, null=True)  # Add join_date field
     location = models.CharField(max_length=255, blank=True, null=True)  # Add location field
 
