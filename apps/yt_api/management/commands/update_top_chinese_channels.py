f206bcfd77f427af256a6305605a258026af82df
impanyu <impanyu@gmail.com>
2024-06-19 16:39:27 -0500

    test

--- apps/yt_api/management/commands/update_top_chinese_channels.py
+++ apps/yt_api/management/commands/update_top_chinese_channels.py
@@ -122,13 +122,9 @@ class Command(BaseCommand):
              last_updated = datetime.strptime(channel_subs[channel_id]["initial_date"],"%Y-%m-%d")
 
              # Add the timedelta to the last_updated date
-             last_updated += random_timedelta
+             #last_updated += random_timedelta
              estimated_subs = int(channel_subs[channel_id]["subscribers"] * 10000)
-             if estimated_subs >1000000:
-                estimated_subs += random.randint(0, 10000)
-             else:
-                estimated_subs += random.randint(0, 1000)
-                 
+             estimated_subs -= random.randint(0, 1000)  # Subtract a random number between 0 and 2000
 
              ChannelSubscribers.objects.create(
                 channel_id=channel['channel_id'],
