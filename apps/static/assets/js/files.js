2a243041783dc606d631ced2fe9f770e4ab0882f
impanyu <impanyu@gmail.com>
2024-08-07 13:40:41 -0500

    test

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -469,6 +469,9 @@ $('#preloader3')[0].style.display = "block";
                                 //console.info(percentComplete);
                                 //$('#preloader3').text('Uploading: ' + Math.round(percentComplete * 100) + '%');
                                 //if (percentComplete < 0.7)
+                                console.info(percentComplete * 100);
+                                console.info(k);
+                                console.info(files.length);
                                     $('#preloader3')[0].style.width = Math.round(percentComplete * 100*k/files.length) + '%';
                                 //else
                                      //$('#preloader3')[0].style.width = Math.round(((percentComplete-0.7)*0.8+0.7) * 100) + '%';
