23c3fe33b7eea0d5aa5808ce238b9b4fedb2a2ef
impanyu <impanyu@gmail.com>
2024-08-07 15:01:21 -0500

    test

--- apps/static/assets/js/files.js
+++ apps/static/assets/js/files.js
@@ -456,7 +456,7 @@ function upload2(sub_files){
               data: form_data,
               enctype: "multipart/form-data",
               success: function (data) {
-                console.info(data);
+                alert(data);
                
                 total_files_uploaded += sub_files.length;
               }
