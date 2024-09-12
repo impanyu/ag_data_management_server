342094b476c1a3e1f8a960d428ce6210dc718d1c
impanyu <impanyu@gmail.com>
2024-09-12 17:46:17 -0500

    test

--- apps/home/native_tools.py
+++ apps/home/native_tools.py
@@ -67,6 +67,7 @@ def get_JD_token(authorization_code):
     CLIENT_SECRET = os.getenv('JD_CLIENT_SECRET')
 
     #TOKEN_GRANT_URL = cache.get('TOKEN_GRANT_URL')
+    print("in get JD token",flush=True)
 
 
     oauth2_session = OAuth2Session(CLIENT_ID,  redirect_uri=CLIENT_REDIRECT_URI, scope=SCOPES_TO_REQUEST)
