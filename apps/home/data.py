d5f23a70c86ac5b903b59ce498ba3e55e4e28806
impanyu <impanyu@gmail.com>
2025-05-01 14:43:08 -0500

    test

--- apps/home/data.py
+++ apps/home/data.py
@@ -854,7 +854,8 @@ def generate_meta_data_for_dir(dir_path,upstream):
     if os.path.exists(os.path.join(settings.CORE_DIR, 'data', parent_meta_data_file_name)):
         with open(os.path.join(settings.CORE_DIR, 'data', parent_meta_data_file_name), "r") as parent_meta_data_file:
             parent_meta_data = json.load(parent_meta_data_file)
-            meta_data["public"] = parent_meta_data["public"]
+            if "public" in parent_meta_data:
+                meta_data["public"] = parent_meta_data["public"]
 
     meta_data["name"] = dir_path.split("/")[-1]
     meta_data["realtime"] = "Non-Realtime"
