b98581e23602364e00fcf996990f8ff1f126e867
impanyu <impanyu@gmail.com>
2025-07-24 14:58:19 -0500

    test

--- core/urls.py
+++ core/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path('accounts/', include('allauth.urls')),
     path('yt/', include('apps.yt_api.urls')),
-    path("", include("apps.home.urls")),      # UI Kits Html files - MOVED TO FIRST
-    path("", include("apps.authentication.urls"))  # Auth routes - login / register - MOVED TO SECOND
+    path("", include("apps.authentication.urls")),  # Auth routes - login / register - FIRST
+    path("", include("apps.home.urls"))      # UI Kits Html files - SECOND
 ]
 
 #urlpatterns += router.urls
